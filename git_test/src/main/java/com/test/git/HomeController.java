4ed90d786370f4f12a64636ec6742e51cfbf21e2
PC-09 <PC-09@DESKTOP-RQDQLQC>
2020-10-06 11:23:16 +0900

    gkk

--- git_test/src/main/java/com/test/git/HomeController.java
+++ git_test/src/main/java/com/test/git/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		Date date2 = new Date();
 		
-		model.addAttribute("server", date2);
+		model.addAttribute("serverTime", date2.toString());
 		
 		return "home";
 	}
